7f0a556d25265ec38568bffa61f9c15fb043fa3a
aghp81 <aghp81@gmail.com>
2025-08-02 21:40:56 +0330

    توضیحات فایل ها

--- handlers/text_handler.py
+++ handlers/text_handler.py
@@ -1,6 +1,7 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
+# اگر متن بفرستی → همون متن رو برمی‌گردونه
 async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
     await update.message.reply_text(f"پیام متنی شما دریافت شد:\n{user_message}")
--- handlers/voice_handler.py
+++ handlers/voice_handler.py
@@ -1,5 +1,6 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
+# اگر ویس بفرستی → پیام تأیید ارسال می‌کنه
 async def handle_voice(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("پیام صوتی شما دریافت شد ✅")
